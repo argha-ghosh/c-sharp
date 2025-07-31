bd37648c3bac36e7269dbc4d1f80fa42da56680d
ARGHA GHOSH <23-50804-1@student.aiub.edu>
2025-07-31 21:51:27 +0600

    Add files via upload

---
+++ Normal Name, ID print/ConsoleAppClassE/Program.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppClassE
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //AddressFormat a1 = new AddressFormat(12, 27, 1889, "Khulna");
+            //AddressFormat a2 = new AddressFormat(42, 198, 1110, "Bogura");
+
+            Student s1 = new Student();
+            s1.SetId(100); //s1.id = 100;
+            s1.SetName("Bruce"); //s1.name = "bruce";
+            s1.SetCgpa(3.12); //s1.cgpa = 3.12;
+            s1.SetBloodGroup("B+"); // s1.bloodGroup = "B+";
+            s1.SetAddress(new AddressFormat(12, 27, 1889, "Khulna"));
+            s1.PrintStudentInfo();
+
+            Student s2 = new Student(200, "clerk", 2.28, "AB+", new AddressFormat(42, 198, 1110, "Bogura"));
+            s2.PrintStudentInfo();
+
+            //constant pointer, [0]
+            //1d, Multi D, jagged array
+
+            int[] ax = new int[5] { 12, 98, -67, 100, 4 };
+
+            //for (int q = 0; q < ax.Length; q++)
+            //{
+            //    Console.Write("{0} ", ax[q]);
+            //}
+            //Console.WriteLine();
+
+            //int k = 0;
+            //while (k < ax.Length)
+            //{
+            //    Console.Write("{0} ", ax[k]);
+            //    k++;
+            //}
+
+            //k = 0;
+            //do
+            //{
+            //    Console.Write("{0} ", ax[k]);
+            //    k++;
+            //}
+            //while (k < 5);
+
+            //foreach (int e in ax)
+            //{
+            //    Console.Write("{0} ", e);
+            //}
+
+            int[,] bx = new int[4, 3] { { 1, 2, 3 }, { 4, 5, 6 }, { 7, 8, 9 }, { 10, 11, 12 } };
+
+            //int r = 0, c;
+            //while(r < 4)
+            //{
+            //    c = 0;
+            //    while(c<3)
+            //    {
+            //        Console.Write("{0} ", bx[r, c]);
+            //        c++;
+            //    }
+            //    Console.WriteLine();
+            //    r++;
+            //}
+
+            //int[,,,] cx = new int[3, 2, 4, 8];
+
+            //foreach (int e in bx)
+            //{
+            //    Console.Write("{0} ", e);
+            //}
+            //Console.WriteLine();
+
+            //int[][] jx = new int[4][];
+            //jx[0] = new int[3] { 1, 2, 3 };
+            //jx[1] = new int[2] { 4, 5 };
+            //jx[2] = new int[5] { 6, 7, 8, 9, 10 };
+            //jx[3] = new int[1] { 11 };
+            //int r = 0, c;
+            //while (r < jx.Length)
+            //{
+            //    c = 0;
+            //    while (c < jx[r].Length)
+            //    {
+            //        Console.Write("{0} ", jx[r][c]);
+            //        c++;
+            //    }
+            //    Console.WriteLine();
+            //    r++;
+            //}
+        }
+    }
+}
---
+++ Normal Name, ID print/ConsoleAppClassE/Student.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppClassE
+{
+    struct AddressFormat
+    {
+        private byte houseNo;
+        private byte roadNo;
+        private ushort postalCode;
+        private string district;
+
+        public AddressFormat(byte houseNo, byte roadNo, ushort postalCode, string district)
+        {
+            this.houseNo = houseNo;
+            this.roadNo = roadNo;
+            this.postalCode = postalCode;
+            this.district = district;
+        }
+
+        public void PrintAddress()
+        {
+            Console.WriteLine("House No: {0}", this.houseNo);
+            Console.WriteLine($"Road No: {roadNo}");
+            Console.WriteLine("Postal Code: {0}", this.postalCode);
+            Console.WriteLine("District: {0}\n", this.district);
+        }
+    }
+
+    class Student
+    {
+        private int id;
+        private string name;
+        private double cgpa;
+        private string bloodGroup;
+        private AddressFormat address;
+
+        public int GetId()
+        {
+            return this.id;
+        }
+
+        public void SetId(int id)
+        {
+            this.id = id;
+        }
+
+        public string GetName()
+        {
+            return this.name;
+        }
+
+        public void SetName(string name)
+        {
+            this.name = name;
+        }
+
+        public double GetCgpa()
+        {
+            return this.cgpa;
+        }
+
+        public void SetCgpa(double cgpa)
+        {
+            if (cgpa >= 0 && cgpa <= 4.0)
+                this.cgpa = cgpa;
+            else
+                this.cgpa = -1;
+        }
+
+        public string GetBloodGroup()
+        {
+            return this.bloodGroup;
+        }
+
+        public void SetBloodGroup(string bloodGroup)
+        {
+            this.bloodGroup = bloodGroup;
+        }
+
+        public AddressFormat GetAddress()
+        {
+            return this.address;
+        }
+
+        public void SetAddress(AddressFormat address)
+        {
+            this.address = address;
+        }
+
+        public Student()
+        {
+        }
+
+        public Student(int id, string name, double cgpa, string bloodGroup, AddressFormat address)
+        {
+            this.SetId(id); //this.id = id;
+            this.SetName(name);//this.name = name;
+            this.SetCgpa(cgpa);//this.cgpa = cgpa;
+            this.SetBloodGroup(bloodGroup);
+            this.SetAddress(address);//this.bloodGroup = bloodGroup;
+        }
+
+        public string FindGradeLetter()
+        {
+            if (this.GetCgpa() >= 3.5 && this.GetCgpa() <= 4.0)
+                return "Student Grade Letter: A+"; //Console.WriteLine("Student Grade Letter: A+");
+            else if (this.GetCgpa() >= 3.0 && this.GetCgpa() <= 3.49)
+                return "Student Grade Letter: A"; //Console.WriteLine("Student Grade Letter: A");
+            else if (this.GetCgpa() >= 2.0 && this.GetCgpa() < 3.0)
+                return "Student Grade Letter: D+"; //Console.WriteLine("Student Grade Letter: D+");
+            else if (this.GetCgpa() >= 0.0 && this.GetCgpa() < 2.0)
+                return "Student Grade Letter: F"; //Console.WriteLine("Student Grade Letter: F");
+            else
+                return "Student Grade Letter: Invalid"; //Console.WriteLine("Student Grade Letter: Invalid");
+        }
+
+        public void PrintStudentInfo()
+        {
+            Console.WriteLine("Student Id: {0}", this.GetId()); //this.id);
+            Console.WriteLine("Student Name: {0}", this.GetName()); //this.name);
+            Console.WriteLine("Student Cgpa: {0}", this.GetCgpa()); //this.cgpa);
+            Console.WriteLine("{0}", this.FindGradeLetter());
+            Console.WriteLine("Student Blood Group: {0}", this.GetBloodGroup()); //this.bloodGroup);
+            this.GetAddress().PrintAddress(); //this.address.PrintAddress();
+        }
+    }
+}
